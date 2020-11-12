fc56cf418bc8547ec8192950fbf916a2f6377f0a
huanghui <815189517@qq.com>
2020-11-12 11:33:31 +0800

    'LiftingStateUp'

---
+++ src/LiftingStateUp/Calculator.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+
+import BoilingVerdict from './BoilingVerdict';
+import TemperatureInput from "./TemperatureInput";
+
+class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
+    this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
+    this.state = {
+      temperature: "",
+      scale: "c",
+    };
+  }
+
+  handleCelsiusChange(temperature) {
+    this.setState({
+      temperature,
+      scale: "c",
+    });
+  }
+
+  handleFahrenheitChange(temperature) {
+    this.setState({
+      temperature,
+      scale: "f",
+    });
+  }
+
+  toCelsius(fahrenheit) {
+    return ((fahrenheit - 32) * 5) / 9;
+  }
+
+  toFahrenheit(celsius) {
+    return (celsius * 9) / 5 + 32;
+  }
+
+  tryConvert(temperature, convert) {
+    const input = parseFloat(temperature);
+    if (Number.isNaN(input)) {
+      return "";
+    }
+    const output = convert(input);
+    const rounded = Math.round(output * 1000) / 1000;
+    return rounded.toString();
+  }
+
+  render() {
+    const scale = this.state.scale;
+    const temperature = this.state.temperature;
+    const celsius =
+      scale === "f" ? this.tryConvert(temperature, this.toCelsius) : temperature;
+    const fahrenheit =
+      scale === "c" ? this.tryConvert(temperature, this.toFahrenheit) : temperature;
+
+    return (
+      <div>
+        <TemperatureInput
+          scale="c"
+          temperature={celsius}
+          onTemperatureChange={this.handleCelsiusChange}
+        />
+        <TemperatureInput
+          scale="f"
+          temperature={fahrenheit}
+          onTemperatureChange={this.handleFahrenheitChange}
+        />
+        <BoilingVerdict
+          celsius={parseFloat(celsius)}
+        />
+      </div>
+    );
+  }
+}
+
+export default Calculator;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 // import App from "./App.js";
 // import App from './UncontrolledComponents';
-import App from './LottieDemo';
+// import App from './LottieDemo';
+import App from './LiftingStateUp/Calculator';
 ReactDOM.render(<App />, document.getElementById("root"));
